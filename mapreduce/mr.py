8d7368e97baecd20b7304a4a8328d222d3afe570
Denny Zheng <zyxdenny@sjtu.edu.cn>
2022-07-28 19:40:23 +0800

    args added

--- mapreduce/mr.py
+++ mapreduce/mr.py
@@ -1,8 +1,22 @@
 from mrjob.job import MRJob
 from mrjob.job import MRStep
-        
+
 
 class BFS(MRJob):
+    def configure_args(self):
+        super(BFS, self).configure_args()
+        self.add_passthru_arg('-f', '--function',type=int, choices=[0,1],help='select function for the program. 0 for getting similar artists within a given distance. 1 for getting the distance between two artists')
+        self.add_passthru_arg('-i', '--input_id', type=str,
+                                help='set the input artist id')
+        self.add_passthru_arg('-o', '--output_id', type=str,
+                                help='set the output artist id')
+        self.add_passthru_arg('-m', '--max_round', type=int, default = 10,
+                                help='set the maximum round of searching')
+        self.add_passthru_arg('-d', '--distance', type=int, default = 2,
+                                help='set the expected distance')
+
+
+
     def mapper(self, _, line):
         v = line.strip().split('|')
         id = v[0]
@@ -12,8 +26,9 @@ class BFS(MRJob):
         dist = int(v[2])
         status = int(v[3])
 
-        if status == 1:
+        if id == self.options.input_id:
             status = -1
+            dist = 0
             for m_id in adj:
                 m_dist = dist + 1
                 m_adj = []
@@ -75,22 +90,47 @@ class BFS(MRJob):
         yield dist, id
 
 
-    def reducer_final(self, key, values):
+    def reducer_final0(self, key, values):
         dist = key
         id_list = []
-        if dist <= 10:
+        if dist == self.options.distance:
             for id in values:
                 id_list.append(id)
             
             yield dist, id_list
+
+    
+    def reducer_final1(self, key, values):
+        if key == self.options.input_id:
+            id = key
+            dist = 9999
+            for node in values:
+                dist_tmp = node[1]
+                status_tmp = node[2]
+
+                if status_tmp == -1:
+                    dist = dist_tmp
+                    break
+                else:
+                    if dist_tmp < dist:
+                        dist = dist_tmp
+            
+            yield id, dist
     
 
     def steps(self):
         process_list = [MRStep(mapper=self.mapper, reducer=self.reducer)]
-        for i in range(9):
-            process_list.append(MRStep(mapper=self.mapper_from_output, reducer=self.reducer))
+        if self.options.function == 0:
+            for i in range(self.options.distance - 1):
+                process_list.append(MRStep(mapper=self.mapper_from_output, reducer=self.reducer))
 
-        process_list.append(MRStep(mapper=self.mapper_final, reducer=self.reducer_final))
+            process_list.append(MRStep(mapper=self.mapper_final, reducer=self.reducer_final0))
+        else:
+            for i in range(self.options.max_round - 2):
+                process_list.append(MRStep(mapper=self.mapper_from_output, reducer=self.reducer))
+            
+            process_list.append(MRStep(mapper=self.mapper_from_output, reducer=self.reducer_final1))
+
         return process_list
 
 
